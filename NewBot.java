6666ffecf860aa3794f404778571694b17a1a5f5
Tietokone <wtl4112@rit.edu>
2011-11-13 14:16:23 -0500

    Initial Commit

---
+++ NewBot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class NewBot implements Bot {
+	public static void main(String[] args) {
+		Ants.run(new NewBot());
+	}
+
+
+	// bot logic, run once per turn
+	public void do_turn(Ants ants) {
+
+	}
+}
